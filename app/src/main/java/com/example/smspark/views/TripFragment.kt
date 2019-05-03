57b89041594bdd3d775b782056a63e347caeea07
asegestam <abbesegestam@gmail.com>
2019-05-03 14:08:11 +0200

    TripFragment 2.0

--- app/src/main/java/com/example/smspark/views/TripFragment.kt
+++ app/src/main/java/com/example/smspark/views/TripFragment.kt
@@ -2,41 +2,43 @@ package com.example.smspark.views
 
 
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Intent
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.smspark.R
 import com.example.smspark.viewmodels.ZoneViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.mapbox.api.geocoding.v5.models.CarmenFeature
 import com.mapbox.geojson.Feature
 import com.mapbox.geojson.Point
 import com.mapbox.geojson.Polygon
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.geometry.LatLngBounds
-import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete
 import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.ui.PlaceAutocompleteFragment
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.ui.PlaceSelectionListener
+import kotlinx.android.synthetic.main.destination_search.*
 import kotlinx.android.synthetic.main.fragment_trip.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 
-class TripFragment : Fragment(), OnMapReadyCallback {
+class TripFragment : Fragment() {
 
     private val FROM_TEXT_VIEW = 1
     private val DESTINATION_TEXT_VIEW = 2
     private var fromLatLng: String? = null
-    private var destinationLatLng: String? = null
-
+    private var toLatLng: String? = null
+    private lateinit var autoCompleteFragment: PlaceAutocompleteFragment
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     //lazy inject ViewModel
     private val zoneViewModel: ZoneViewModel by sharedViewModel()
 
@@ -48,47 +50,97 @@ class TripFragment : Fragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        autoCompleteFragment = PlaceAutocompleteFragment.newInstance(getString(R.string.access_token), PlaceOptions.builder()
+                .language("sv")
+                .hint(getString(R.string.search_hint))
+                .country("SE")
+                .proximity(getUserLocation())
+                .build(PlaceOptions.MODE_CARDS))
         initComponents()
     }
 
-    override fun onMapReady(mapboxMap: MapboxMap) {}
+
+    private fun initAutoComplete(fragment: PlaceAutocompleteFragment, tag: String) {
+        fragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+                override fun onPlaceSelected(carmenFeature: CarmenFeature) {
+                    when(tag) {
+                        "to" -> handleToLocation(carmenFeature)
+                        "from" -> handleFromLocation(carmenFeature)
+                    }
+                    removeAutoCompleteFragment()
+                }
+                override fun onCancel() {
+                    removeAutoCompleteFragment()
+                }
+            })
+    }
+
+    private fun addAutoCompleteFragment(fragment: PlaceAutocompleteFragment, tag: String) {
+        activity?.supportFragmentManager?.let {
+            if(it.findFragmentByTag(tag) == null) {
+                Log.d(TAG, "adding fragment" + fragment.toString())
+                val transaction = it.beginTransaction()
+                transaction.add(R.id.fragment_container, fragment, tag)
+                transaction.commit()
+                initAutoComplete(fragment, tag)
+            } else {
+                removeAutoCompleteFragment()
+            }
+        }
+    }
+
+    private fun removeAutoCompleteFragment() {
+        activity?.supportFragmentManager?.let {
+            println(it.fragments)
+            val fragment = if (it.findFragmentByTag("to") != null) it.findFragmentByTag("to") else it.findFragmentByTag("from")
+            if(fragment != null) {
+                Log.d(TAG, "removing fragment" + fragment.toString())
+                val transaction = it.beginTransaction()
+                transaction.remove(fragment)
+                transaction.commit()
+            }
+        }
+    }
 
     @SuppressLint("MissingPermission")
     private fun initComponents(){
 
-        /*
-        textFrom.setOnClickListener { startAutoCompleteActivity(FROM_TEXT_VIEW) }
-        textDestination.setOnClickListener { startAutoCompleteActivity(DESTINATION_TEXT_VIEW) }
-        iv_my_location.setOnClickListener {
-            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-            fusedLocationClient.lastLocation
-                    .addOnSuccessListener { location: Location? ->
-                        // Got last known location. In some rare situations this can be null.
-                        if ((location != null) && (textDestination.text.toString() != "Current Location")) {
-                            textFrom.text = "Current Location"
-                            val point = Point.fromLngLat(location.longitude, location.latitude)
-                            fromLatLng = point.toJson().toString()
-                        }
-                    }
+        toLocation.setOnClickListener { addAutoCompleteFragment(autoCompleteFragment, "to") }
+        fromLocation.setOnClickListener { addAutoCompleteFragment(autoCompleteFragment, "from") }
+
+        myLocation.setOnClickListener {
+            if (fromLocation.text != getString(R.string.nuvarande_plats)) {
+                fromLocation.text = getString(R.string.nuvarande_plats)
+                val location = getUserLocation()
+                fromLatLng = location.toJson().toString()
+            }
         }
 
-        iv_swap.setOnClickListener {
-            if ((fromLatLng != null) && (destinationLatLng != null)){
-                val tempText = textFrom.text.toString()
+        swapIcon.setOnClickListener {
+            if ((fromLatLng != null) && (toLatLng != null)){
+                val tempText = fromLocation.text
                 val tempPoint = fromLatLng
-                textFrom.text = textDestination.text
-                textDestination.text = tempText
-                fromLatLng = destinationLatLng
-                destinationLatLng = tempPoint
+                fromLocation.text = toLocation.text
+                toLocation.text = tempText
+                fromLatLng = toLatLng
+                toLatLng = tempPoint
             } else {
                 Toast.makeText(requireContext(), "Can´t swap", Toast.LENGTH_LONG).show()
             }
         }
-        */
         initButtons()
-        initSpinner()
         initObservables()
+    }
+
+    private fun handleFromLocation(carmenFeature: CarmenFeature) {
+        fromLatLng = carmenFeature.geometry()?.toJson()
+        fromLocation.text = carmenFeature.text()
+    }
+
+    private fun handleToLocation(carmenFeature: CarmenFeature) {
+        toLatLng = carmenFeature.geometry()?.toJson()
+        toLocation.text = carmenFeature.text()
     }
 
     private fun initObservables(){
@@ -116,71 +168,19 @@ class TripFragment : Fragment(), OnMapReadyCallback {
         })
     }
 
-    private fun initSpinner(){
-        val arrayAdapter = ArrayAdapter<String>(requireContext(),
-                R.layout.support_simple_spinner_dropdown_item,
-                resources.getStringArray(R.array.vehicles))
-
-        arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
-
-        //spinner.adapter = arrayAdapter
-    }
-
-    /** Starts a Search AutoComplete activity for searching locations */
-    private fun startAutoCompleteActivity(id: Int) {
-        val intent = PlaceAutocomplete.IntentBuilder()
-                .accessToken(getString(R.string.access_token))
-                .placeOptions(PlaceOptions.builder()
-                        .language("sv")
-                        .country("SE")
-                        .proximity(Point.fromLngLat(11.9745, 57.7088))
-                        .build(PlaceOptions.MODE_CARDS))
-                .build(requireActivity())
-        startActivityForResult(intent, id)
-    }
-
-    @SuppressLint("MissingPermission")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        //if result code is for AutoComplete activity
-        if (resultCode == Activity.RESULT_OK && (requestCode == FROM_TEXT_VIEW || requestCode == DESTINATION_TEXT_VIEW)) {
-            handleAutoCompleteResult(data, requestCode)
-        }
-    }
-
-    /** Handles the result given from the Search AutoComplete Activity*/
-    private fun handleAutoCompleteResult(data: Intent?, id: Int) {
-        //Gets the place data from searched position
-        val feature = PlaceAutocomplete.getPlace(data)
-        val point = feature?.geometry() as Point
-
-        when(id){
-            FROM_TEXT_VIEW -> {
-                //textFrom.text = feature.placeName()
-                fromLatLng = point.toJson().toString()
-            }
-            DESTINATION_TEXT_VIEW -> {
-                //textDestination.text = feature.placeName()
-                destinationLatLng = point.toJson().toString()
-            }
-        }
-
-        //Toast.makeText(requireContext(), point.toString(), Toast.LENGTH_LONG).show()
-    }
-
     private fun getNearestParking() {
-        if (destinationLatLng != null) {
-            val destinationPoint = Point.fromJson(destinationLatLng!!)
+        if (toLatLng != null) {
+            val destinationPoint = Point.fromJson(toLatLng!!)
             zoneViewModel.getSpecificZones(destinationPoint.latitude(), destinationPoint.longitude(), 500)
         }
     }
 
     private fun checkArguments(wayPoint : Point, feature : Feature){
-        if(fromLatLng != null && destinationLatLng != null){
+        if(fromLatLng != null && toLatLng != null){
             val bundle = Bundle()
 
             bundle.putString("fromArg", fromLatLng)
-            bundle.putString("destArg", destinationLatLng)
+            bundle.putString("destArg", toLatLng)
             bundle.putString("wayPointArg", wayPoint.toJson())
             bundle.putString("wayPointFeatureArg", feature.toJson())
             //Toast.makeText(requireContext(), wayPoint.toString(), Toast.LENGTH_LONG).show()
@@ -191,14 +191,33 @@ class TripFragment : Fragment(), OnMapReadyCallback {
 
     private fun initButtons() {
         next_btn.setOnClickListener {
-            if(fromLatLng != null && destinationLatLng != null)
+            if(fromLatLng != null && toLatLng != null)
                 getNearestParking()
             else
                 Toast.makeText(requireContext(), "Choose all required alternatives", Toast.LENGTH_LONG).show()
         }
     }
 
+    @SuppressLint("MissingPermission")
+    private fun getUserLocation(): Point {
+        var point: Point = Point.fromLngLat(57.0, 12.0)
+        fusedLocationProviderClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    location?.let {
+                        point = Point.fromLngLat(location.longitude, location.latitude)
+
+                    }
+                }
+        return point
+    }
+
+
     companion object {
         val TAG : String = "TripFragment"
+    }
+
+    override fun onPause() {
+        removeAutoCompleteFragment()
+        super.onPause()
     }
 }
