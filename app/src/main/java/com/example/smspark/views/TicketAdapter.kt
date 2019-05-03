0b416de7171d7d2a9c24fa098bb3ea95c9f92197
Markus <markus.saarijarvi@hotmail.com>
2019-05-03 12:56:10 +0200

    Första steget med att skapa en recyclerview med gamla parkeringsbiljetter

---
+++ app/src/main/java/com/example/smspark/views/TicketAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.smspark.views
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smspark.R
+
+class TicketAdapter(val tickets: ArrayList<String>) : RecyclerView.Adapter<TicketAdapter.TicketViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TicketAdapter.TicketViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.ticket_list_item, parent, false)
+        return TicketViewHolder(view)
+    }
+
+    override fun getItemCount() = tickets.size
+
+    override fun onBindViewHolder(holder: TicketAdapter.TicketViewHolder, position: Int) {
+        holder.ticket.text = tickets[position]
+    }
+
+    class TicketViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val ticket: TextView = itemView.findViewById<TextView>(R.id.ticketText)
+    }
+}
--- app/src/main/java/com/example/smspark/views/TicketsFragment.kt
+++ app/src/main/java/com/example/smspark/views/TicketsFragment.kt
@@ -6,9 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.smspark.R
+import com.mapbox.geojson.Feature
+import kotlinx.android.synthetic.main.fragment_map.*
+import kotlinx.android.synthetic.main.fragment_tickets.*
 
 class TicketsFragment : Fragment() {
+
+    //RecyclerView fields
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var ticketAdapter: TicketAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +32,26 @@ class TicketsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_tickets, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+
+        val tickets: ArrayList<String> = ArrayList()
+
+        for (i in 1..100){
+            tickets.add("Parking #$i")
+        }
+
+        recyclerView = ticket_recycler_view
+        ticketAdapter = TicketAdapter(tickets)
+        recyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        recyclerView.adapter = ticketAdapter
+        val snapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(recyclerView)
+    }
 
     companion object {
         val TAG : String = "TicketsFragment"
